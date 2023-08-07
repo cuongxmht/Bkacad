da60995502e655bf8f0689ee313f59ab3bc10e88
cuongxmht <cuongxmht@gmail.com>
2023-08-07 16:39:46 +0700

    update

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string strA="123", strB="432";
+
+int.TryParse(strA, out int a);
+
+int.TryParse(strB, out int b);
+
+System.Console.WriteLine($"{strA} + {strB} = {a+b}");
