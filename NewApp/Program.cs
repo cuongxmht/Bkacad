1151ccfba7c9a756d3bed23cb3127a9103465c7f
cuongxmht <cuongxmht@gmail.com>
2023-08-08 08:33:20 +0700

    luong

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-string strA="123", strB="432";
+// string strA="123", strB="432";
 
-int.TryParse(strA, out int a);
+// int.TryParse(strA, out int a);
 
-int.TryParse(strB, out int b);
+// int.TryParse(strB, out int b);
 
-System.Console.WriteLine($"{strA} + {strB} = {a+b}");
+// System.Console.WriteLine($"{strA} + {strB} = {a+b}");
+
+//Luong = LCB*HeSo + PC
+int LCB=1790000, PC=2000000;
+float HeSo=3.25F;
+System.Console.WriteLine($"LCB={LCB}, HeSo={HeSo}, PC={PC}");
+System.Console.WriteLine($"Luong = LCB*HeSo + PC");
+System.Console.WriteLine($"Luong = {LCB*HeSo + PC}");
