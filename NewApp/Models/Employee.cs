c68cdc244de3babe9955a9b8c6fa66d002994923
cuongxmht <cuongxmht@gmail.com>
2023-08-08 11:31:06 +0700

    luong

--- NewApp/Models/Employee.cs
+++ NewApp/Models/Employee.cs
@@ -4,7 +4,16 @@ namespace NewApp.Models
     public class Employee{
         public String HoTen { get; set; }
         public string  DiaChi { get; set; }
-
+        public int NamSinh { get; set; }
+public Employee(string ht,string dc)
+{
+    HoTen=ht;
+    DiaChi=dc;
+}
+public Employee()
+{
+    
+}
         public void NhapThongTin()
         {
             System.Console.Write("Ho ten: ");
@@ -12,13 +21,19 @@ namespace NewApp.Models
 
             System.Console.Write("Dia chi: ");
             this.DiaChi = Console.ReadLine();
-
+            System.Console.Write("Nam: ");
+            this.NamSinh = int.Parse(Console.ReadLine());
         }
 
         public void HienThi()
         {
             System.Console.WriteLine("------");
             System.Console.WriteLine($"Nhan vien: {HoTen} - {DiaChi}");
+            System.Console.WriteLine($"Tuoi: {TinhTuoi()}");
+        }
+        public int TinhTuoi()
+        {
+            return DateTime.Now.Year - NamSinh;
         }
     }
 }
