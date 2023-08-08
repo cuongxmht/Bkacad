9c1a468fd3441e9aea7124decd32aa7c9e5a0d97
cuongxmht <cuongxmht@gmail.com>
2023-08-08 09:05:25 +0700

    luong

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -12,6 +12,20 @@
 //Luong = LCB*HeSo + PC
 int LCB=1790000, PC=2000000;
 float HeSo=3.25F;
+string hs;
+while(true){
+    System.Console.Write("Nhap he so luong: ");
+    hs=Console.ReadLine();
+    if(!float.TryParse(hs, out HeSo) )
+    {
+        System.Console.WriteLine("He so luong nhap khong dung.");
+    } 
+    else if(HeSo<=0)
+        System.Console.WriteLine("He so luong phai >0.");
+    else if(HeSo>0) break;
+    System.Console.WriteLine("");
+}
+
 System.Console.WriteLine($"LCB={LCB}, HeSo={HeSo}, PC={PC}");
 System.Console.WriteLine($"Luong = LCB*HeSo + PC");
 System.Console.WriteLine($"Luong = {LCB*HeSo + PC}");
