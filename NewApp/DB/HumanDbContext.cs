481e640187967b4ff3fc1c350f1b45b98b1df73c
cuongxmht <cuongxmht@gmail.com>
2023-08-09 15:21:31 +0700

    update

--- NewApp/DB/HumanDbContext.cs
+++ NewApp/DB/HumanDbContext.cs
@@ -10,22 +10,28 @@ namespace NewApp.DB
 {
     public class HumanDbContext: DbContext
     {
-        readonly IConfiguration _configuration;
-        public HumanDbContext(IConfiguration configuration)
-        {
-            _configuration=configuration;
-        }
-
+        //readonly IConfiguration _configuration;
+        // public HumanDbContext(IConfiguration configuration)
+        // {
+        //     _configuration=configuration;
+        // }
+public HumanDbContext(DbContextOptions<HumanDbContext> options) : base()
+{
+    
+}
         public DbSet<Person> Persons{ get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(_configuration.GetConnectionString("DataBase"));
+            // optionsBuilder.UseSqlite(_configuration.GetConnectionString("DataBase"));
+            optionsBuilder.UseSqlite("Data Source=LocalDatabase.db");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasDefaultSchema("apps");
+            //modelBuilder.HasDefaultSchema("apps");
             modelBuilder.Entity<Person>().ToTable("Person");
-            // base.OnModelCreating(modelBuilder);
+            
+            
+            base.OnModelCreating(modelBuilder);
         }
 
     }
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -4,8 +4,11 @@ using NewApp.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using NewApp.DB;
+using System.Collections;
 
-var configurations=new ConfigurationBuilder().AddJsonFile("appsettings.json");
+var configurations=new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json");
 var config=configurations.Build();
 var connectionString=config.GetConnectionString("Database");
 
@@ -57,6 +60,7 @@ student.CreateNew();
 student.ShowInfo();
 */
 //Mang, ds
+/*
 System.Console.Write("Nhap so phan tu: ");
 int n=Convert.ToInt32(Console.ReadLine());
 Person[] arrPerson=new Person[n];
@@ -73,8 +77,38 @@ for(int i=0;i<n;i++)
     System.Console.WriteLine($"-----{i+1}------");
     arrPerson[i].ShowInfo();
 }
+*/
+//Array list
+System.Console.Write("Nhap so phan tu: ");
+int n;
+while(true)
+{
+    if(int.TryParse(Console.ReadLine(),out n) && n>0)
+    break;
+}
 
+ArrayList arrPerson=new ArrayList();
+for(int i=0;i<n;i++)
+{
+    System.Console.WriteLine($"{i+1}");
+    Person person=new Person();
+    person.CreateNew();
+    arrPerson.Add(person);
+}
+System.Console.WriteLine("\nHien thi thong tin");
+for(int i=0;i<n;i++)
+{
+    System.Console.WriteLine($"-----{i+1}------");
+    var person=arrPerson[i] as Person;
+    person.ShowInfo();
+}
 
-
-
+var serviceProvider=services.BuildServiceProvider();
+var scope=serviceProvider.CreateScope();
+var db=scope.ServiceProvider.GetService<HumanDbContext>();
+db.Persons.Add(new Person{
+    Name="abc",Address="xys",YearOfBirth=1990,Id=Guid.NewGuid()
+});
+System.Console.WriteLine("\n\nHIEN THI THONG TIN TU DB");
+db.Persons?.FirstOrDefault()?.ShowInfo();
 
